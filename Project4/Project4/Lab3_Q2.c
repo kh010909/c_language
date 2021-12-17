73363adef7b970ec5071d83c0c81613342c612bd
kh010909 <95903086+kh010909@users.noreply.github.com>
2021-12-17 15:47:22 +0800

    prime number function

---
+++ Project4/Project4/Lab3_Q2.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int prime(int);//宣告
+int main(){
+	int num;//輸入的數
+	printf("Please enter a number,enter negative number to exit:");
+	scanf_s("%d", &num);
+	while(num >= 0){
+		if (prime(num) == 0) {
+		printf("This number is a prime number\n");
+		}
+		else if (prime(num) == 1) {
+		printf("This number is not a prime number\n");
+		}
+		printf("Please enter a number,enter negative number to exit:");
+		scanf_s("%d", &num);
+	}
+	return 0;
+}
+int prime(int num) {
+	int i;
+	for (i = 2; i <= num - 1;++i) {//求2到(輸入的數-1)
+		//如果整除就回傳1(非質數)
+		if ((num % i) == 0) {
+			return 1;
+			break;
+		}
+	}
+	//若沒有i可以整除就回傳0(質數)
+	return 0;
+}
