0d8a2d47cff1f493b5f020d49359846a1019f383
kh010909 <95903086+kh010909@users.noreply.github.com>
2021-12-19 18:21:31 +0800

    Updated pow function

--- Project3/Project3/Lab3_Q1.c
+++ Project3/Project3/Lab3_Q1.c
@@ -1,31 +1,35 @@
 #include<stdio.h>
-float pow(int, int);
+float pow(int, int);//宣告
 int main() {
-	int base, exp;
-	printf("enter a base and exp:");
+	int base, exp;//底數,指數
+	printf("enter a base and exp, base have to be a positive number:");
 	scanf_s("%d %d", &base, &exp);
-	while (base != -1 || exp != -1) {
+	while (base >= 0 || exp != -1) {
 		printf("The expotential is:%.2f\n", pow(base, exp));
 		printf("enter a base and exp:");
 		scanf_s("%d %d", &base, &exp);
 	}
 }
+//pow function
 float pow(int base, int exp) {
 	int i;
 	float sum = 1;
+	//exp大於0時
 	if (exp >= 1) {
 		for (i = 1;i <= exp; i++) {
 			sum *= base;
 		}
 		return sum;
 	}
+	//exp小於0時
 	else if (exp <= -1) {
 		for (i = -1;i >= exp; i--) {
 			sum /= base;
 		}
 		return sum;
 	}
+	//exp等於0時
 	else {
-		return 0;
+		return 1;
 	}
 }
